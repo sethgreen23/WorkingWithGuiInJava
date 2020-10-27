8e0e27a115321048f994daa8cc6ed21ef00302ae
sethgreen23 <dridi.chaith@gmail.com>
2020-10-27 10:52:36 +0100

    First gui

---
+++ FirstGui/src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		//make a frame
+		JFrame frame = new JFrame();
+		//create and add a button to frame
+		JButton button = new JButton("Hello");
+		frame.add(button);
+		//add listener to the closing button
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//give a size to the frame
+		frame.setSize(300,300);
+		//make the frame visible
+		frame.setVisible(true);
+	}
+}
